bf9822a6ef4f9c7e31d39616bfb09674f1a2aed6
Andrew Buckingham <abuckingham99@gmail.com>
2021-01-17 23:38:43 -0800

    Enhancement: submitted movie titles and ratings are displayed in a table

--- movies_app/app.js
+++ movies_app/app.js
@@ -1,11 +1,15 @@
+let reviewList = {};
 
 $("#review-form").on("submit", function(event) {
   event.preventDefault();
   let title = $("#title").val();
   let rating = $("#rating").val();
+  reviewList[0] = title;
+  reviewList[1] = rating;
   this.reset();
-  
-  $("#display-reviews").append(`Movie Title: ${title} -- Rating: ${rating}<br>`)
 
+  let row = $(`<tr><td>${title}</td><td>${rating}</td></tr>`);
+  $("#reviews-table").append(row);
 
 })
+

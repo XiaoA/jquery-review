3214da6e75357c6d6d116e58263ba2d15ab96dae
Andrew Buckingham <abuckingham99@gmail.com>
2021-01-21 13:56:13 -0800

    Feature: User can delete ratings

--- movies_app/app.js
+++ movies_app/app.js
@@ -1,15 +1,25 @@
 let reviewList = {};
+let reviewId = 0;
 
+// Add review
 $("#review-form").on("submit", function(event) {
   event.preventDefault();
   let title = $("#title").val();
   let rating = $("#rating").val();
+  reviewId++
+
   reviewList[0] = title;
   reviewList[1] = rating;
-  this.reset();
+  reviewList[2] = reviewId;
 
-  let row = $(`<tr><td>${title}</td><td>${rating}</td></tr>`);
+  let row = $(`<tr><td>${title}</td><td>${rating}</td><td><button class="delete-button">Delete</button></td></tr>"`);
   $("#reviews-table").append(row);
+  this.reset(); 
+});
 
+// Delete Review
+$("tbody").on("click", ".delete-button", function(event){
+  let deletedRow = $(this).closest("tr").remove();
 })
+
 
